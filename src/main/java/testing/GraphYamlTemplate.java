591c6a4c131a8e6bef863b584eae14dc4c8eaa9d
Boris Milner <boris.milner@gmail.com>
2019-08-17 21:12:18 +0300

    Cleaning

--- src/main/java/testing/GraphYamlTemplate.java
+++ src/main/java/testing/GraphYamlTemplate.java
@@ -9,12 +9,4 @@ public class GraphYamlTemplate {
     public List<Map<String, Map<String, Object>>> nodes;
     public List<Map<String, Object>> relationships;
     public List<Map<String, Map<String, String>>> customProperties;
-
-//    public GraphYamlTemplate(String name, String comments, List<Map<String, NodeDetails>> nodes, RelationsDetails relationships, List<Map<String, Map<String, String>>> customProperties) {
-//        this.name = name;
-//        this.comments = comments;
-//        this.nodes = nodes;
-//        this.relationships = relationships;
-//        this.customProperties = customProperties;
-//    }
 }
