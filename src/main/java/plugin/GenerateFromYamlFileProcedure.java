c216285e12bdc208c0e251617afe60fcfbe38641
Boris Milner <boris.milner@gmail.com>
2019-08-20 18:54:25 +0300

    Refactoring

--- src/main/java/plugin/GenerateFromYamlFileProcedure.java
+++ src/main/java/plugin/GenerateFromYamlFileProcedure.java
@@ -8,7 +8,7 @@ import org.neo4j.procedure.Mode;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.stream.Stream;
 
 @SuppressWarnings("unused")
@@ -22,6 +22,6 @@ public class GenerateFromYamlFileProcedure extends PluginProcedure {
         log.info(String.format("Generating a graph from a YAML template if file: %s", yamlFilePath));
         GraphGenerator graphGenerator = getGraphGenerator();
         graphGenerator.generateFromYamlFile(yamlFilePath);
-        return Stream.of(new ValueListResult(Arrays.asList(new String[]{"Done"})));
+        return Stream.of(new ValueListResult(Collections.singletonList("Done")));
     }
 }
