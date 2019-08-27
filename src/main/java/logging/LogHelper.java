7c2c43cd2fd82c984a0e3d798eb6c957b945afc6
Boris Milner <boris.milner@gmail.com>
2019-08-27 18:15:12 +0300

    Adjusting log-definitions

--- src/main/java/logging/LogHelper.java
+++ src/main/java/logging/LogHelper.java
@@ -1,19 +1,8 @@
 package logging;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.Appender;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.appender.ConsoleAppender;
-import org.apache.logging.log4j.core.config.AppenderRef;
-import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.LoggerConfig;
-import org.apache.logging.log4j.core.layout.PatternLayout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.File;
-import java.net.URI;
+import org.slf4j.impl.SimpleLogger;
 
 public class LogHelper {
     private static Logger logger;
@@ -27,37 +16,12 @@ public class LogHelper {
     }
 
     private static void init() {
+        System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "TRACE");
+        System.setProperty(SimpleLogger.DATE_TIME_FORMAT_KEY, "[yyyy/MM/dd HH:mm:ss]");
+        System.setProperty(SimpleLogger.LEVEL_IN_BRACKETS_KEY, "true");
+        System.setProperty(SimpleLogger.SHOW_DATE_TIME_KEY, "true");
+
         String loggerName = "NeoGoodies";
-
-        LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
-        Configuration config = ctx.getConfiguration();
-        File f = new File("src/main/java/logging/log4j2.xml");
-        URI fc = f.toURI();
-        ctx.setConfigLocation(fc);
-
-        PatternLayout layout = PatternLayout.newBuilder()
-                .withConfiguration(config)
-                .withPattern("%d{HH:mm:ss} [%t] %level - %msg%n")
-                .build();
-
-        Appender appender = ConsoleAppender.newBuilder()
-                .setName(loggerName)
-                .setLayout(layout)
-                .setConfiguration(config)
-                .build();
-
-        appender.start();
-        config.addAppender(appender);
-
-        AppenderRef appenderRef = AppenderRef.createAppenderRef("Console-Appender", null, null);
-        AppenderRef[] appenderRefs = new AppenderRef[]{appenderRef};
-
-        LoggerConfig loggerConfig = LoggerConfig
-                .createLogger(false, Level.DEBUG, loggerName, "true", appenderRefs, null, config, null);
-        loggerConfig.addAppender(appender, Level.DEBUG, null);
-        config.addLogger(loggerName, loggerConfig);
-        ctx.updateLoggers();
-
         logger = LoggerFactory.getLogger(loggerName);
     }
 
