39bc14dfb4602f22e10c5676f7f22a8484f63bd3
Boris Milner <boris.milner@gmail.com>
2019-08-29 17:10:31 +0300

    Kotlinization continues...

--- src/main/kotlin/structures/DataStructuresForResults.kt
+++ src/main/kotlin/structures/DataStructuresForResults.kt
@@ -6,3 +6,5 @@ import org.neo4j.graphdb.Relationship
 data class GraphResult(val nodes: List<Node>, val relationships: List<Relationship>)
 data class NodeListResult(val nodes: List<Node>)
 data class NodeResult(val node: Node)
+data class RelationshipListResult(val relationships: List<Relationship>)
+class ValueListResult(val values: List<Any>)
