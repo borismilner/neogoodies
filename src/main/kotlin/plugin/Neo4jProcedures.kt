1c3e6d318f3ae2fb9a745fc351b747cd38d7bb8c
Boris Milner <boris.milner@gmail.com>
2019-08-29 19:32:47 +0300

    Adjusting for Neo4J's expectations of a plugin

--- src/main/kotlin/plugin/Neo4jProcedures.kt
+++ src/main/kotlin/plugin/Neo4jProcedures.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package plugin
 
 import graph_generator.GraphGenerator
@@ -7,6 +9,7 @@ import org.neo4j.procedure.Context
 import org.neo4j.procedure.Mode
 import org.neo4j.procedure.Name
 import org.neo4j.procedure.Procedure
+import structures.GraphResult
 import structures.NodeListResult
 import structures.ValueListResult
 import utilities.ValueFaker
@@ -75,5 +78,24 @@ class GenerateValuesProcedure : PluginProcedure() {
                 generatorName,
                 parameters as List<Any>, // TODO: Check and assign empty list when an empty string
                 howMany)))
+    }
+}
+
+class GenerateLinkedListProcedure : PluginProcedure() {
+    @JvmField
+    @Context
+    var log: Log? = null
+
+    @Procedure(value = "generate.linkedList", mode = Mode.WRITE)
+    fun generateNodes(@Name("howMany") howMany: Long,
+                      @Name("labels") labelsStringArray: Any,
+                      @Name("nodePropertiesString") nodePropertiesString: String,
+                      @Name("relationshipType") relationshipType: String): Stream<GraphResult> {
+        val labels = (labelsStringArray as ArrayList<String>).toTypedArray()
+        log!!.info("Generating a linked list of $howMany nodes with labels ${labels.contentToString()} connected by $relationshipType")
+        val graphGenerator = graphGenerator
+        val nodesForLinkedList = graphGenerator.generateNodes(GraphGenerator.labelsFromStrings(labels), nodePropertiesString, howMany)
+        val graphResult = graphGenerator.generateLinkedList(nodesForLinkedList, relationshipType)
+        return Stream.of(graphResult)
     }
 }
