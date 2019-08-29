c32543236d8672a325b0751b8bdbb7acc49e0ee6
Boris Milner <boris.milner@gmail.com>
2019-08-29 15:03:10 +0300

    Migration of ValueFaker

--- src/main/java/utilities/ValueFakerOptions.java
+++ src/main/java/utilities/ValueFakerOptions.java
@@ -1,46 +1,46 @@
-package utilities;
-
-public class ValueFakerOptions {
-    // Names
-    public static final String FIRSTNAME = "firstName";
-    public static final String LASTNAME = "lastName";
-    public static final String FULLNAME = "fullName";
-
-    // Address
-    public static final String COUNTRY = "country";
-    public static final String CITY = "city";
-    public static final String STATE = "state";
-    public static final String STREET_ADDRESS = "streetAddress";
-    public static final String STREET_NAME = "streetName";
-    public static final String ZIP_CODE = "zipCode";
-    public static final String LATITUDE = "latitude";
-    public static final String LONGITUDE = "longitude";
-
-    // Business
-    public static final String CREDIT_CARD_NUMBER = "creditCardNumber";
-    public static final String CREDIT_CARD_TYPE = "creditCardType";
-
-    // Company
-    public static final String COMPANY_NAME = "companyName";
-
-    // Internet
-    public static final String AVATAR_URL = "avatarUrl";
-    public static final String EMAIL_ADDRESS = "email";
-    public static final String URL = "url";
-    public static final String IPV4 = "ipv4";
-
-    // Lorem
-    public static final String PARAGRAPH = "paragraph";
-    public static final String SENTENCE = "sentence";
-    public static final String WORD = "word";
-
-    // Phone
-    public static final String PHONE_NUMBER = "phoneNumber";
-
-    // Time
-    public static final String UNIX_TIME = "unixTime";
-
-    // Numbers
-    public static final String NUMBER_BETWEEN = "numberBetween";
-    public static final String RANDOM_NUMBER = "randomNumber";
-}
+//package utilities;
+//
+//public class ValueFakerOptions {
+//    // Names
+//    public static final String FIRSTNAME = "firstName";
+//    public static final String LASTNAME = "lastName";
+//    public static final String FULLNAME = "fullName";
+//
+//    // Address
+//    public static final String COUNTRY = "country";
+//    public static final String CITY = "city";
+//    public static final String STATE = "state";
+//    public static final String STREET_ADDRESS = "streetAddress";
+//    public static final String STREET_NAME = "streetName";
+//    public static final String ZIP_CODE = "zipCode";
+//    public static final String LATITUDE = "latitude";
+//    public static final String LONGITUDE = "longitude";
+//
+//    // Business
+//    public static final String CREDIT_CARD_NUMBER = "creditCardNumber";
+//    public static final String CREDIT_CARD_TYPE = "creditCardType";
+//
+//    // Company
+//    public static final String COMPANY_NAME = "companyName";
+//
+//    // Internet
+//    public static final String AVATAR_URL = "avatarUrl";
+//    public static final String EMAIL_ADDRESS = "email";
+//    public static final String URL = "url";
+//    public static final String IPV4 = "ipv4";
+//
+//    // Lorem
+//    public static final String PARAGRAPH = "paragraph";
+//    public static final String SENTENCE = "sentence";
+//    public static final String WORD = "word";
+//
+//    // Phone
+//    public static final String PHONE_NUMBER = "phoneNumber";
+//
+//    // Time
+//    public static final String UNIX_TIME = "unixTime";
+//
+//    // Numbers
+//    public static final String NUMBER_BETWEEN = "numberBetween";
+//    public static final String RANDOM_NUMBER = "randomNumber";
+//}
