00929b0e789199e904fa67cb3c5b24e631c442e9
Boris Milner <boris.milner@gmail.com>
2019-08-30 12:17:53 +0200

    More unit-testing

--- src/test/kotlin/graph_generator/PluginProcedureTests.kt
+++ src/test/kotlin/graph_generator/PluginProcedureTests.kt
@@ -5,17 +5,29 @@ import org.junit.jupiter.api.*
 import org.neo4j.graphdb.Label
 import org.neo4j.graphdb.Node
 import plugin.GenerateNodesProcedure
+import plugin.GenerateValuesProcedure
 import tools.GraphGenerator
 import utilities.EmbeddedServerHelper
 import utilities.TestUtil
 import utilities.TestUtil.registerProcedure
 import utilities.ValueFaker
 import utilities.YamlParser
-import java.util.*
 
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class PluginProcedureTests {
+
+    private val howManyToCreate = 10
+    private val labelsForEachNode = arrayListOf("Officer", "Gentleman")
+    private val propertiesForEachNode = mapOf(
+            Pair("first_name", "FIRSTNAME"),
+            Pair("last_name", "LASTNAME"),
+            Pair("country", "COUNTRY"),
+            Pair("zip_code", "ZIP_CODE"),
+            Pair("credit_card_type", "CREDIT_CARD_TYPE"),
+            Pair("credit_card_number", "CREDIT_CARD_NUMBER"),
+            Pair("phone_number", "PHONE_NUMBER")
+    )
 
     private lateinit var graphGenerator: GraphGenerator
 
@@ -24,6 +36,7 @@ class PluginProcedureTests {
         val embeddedServer = EmbeddedServerHelper.graphDb
         graphGenerator = GraphGenerator(embeddedServer, YamlParser(), ValueFaker())
         registerProcedure(embeddedServer, GenerateNodesProcedure::class.java)
+        registerProcedure(embeddedServer, GenerateValuesProcedure::class.java)
     }
 
     @BeforeEach
@@ -37,18 +50,7 @@ class PluginProcedureTests {
     }
 
     @Test
-    fun testGenerateNodes() {
-        val howManyToCreate = 10
-        val labelsForEachNode = arrayListOf("Officer", "Gentleman")
-        val propertiesForEachNode = mapOf(
-                Pair("first_name", "FIRSTNAME"),
-                Pair("last_name", "LASTNAME"),
-                Pair("country", "COUNTRY"),
-                Pair("zip_code", "ZIP_CODE"),
-                Pair("credit_card_type", "CREDIT_CARD_TYPE"),
-                Pair("credit_card_number", "CREDIT_CARD_NUMBER"),
-                Pair("phone_number", "PHONE_NUMBER")
-        )
+    fun testGenerateNodesProcedure() {
         val stringBuilder = StringBuilder("{")
         for ((propertyName, generatorName) in propertiesForEachNode) {
             stringBuilder.append("'$propertyName':'$generatorName',")
@@ -71,6 +73,23 @@ class PluginProcedureTests {
                         assertThat(node.hasProperty(propertyName))
                     }
                 }
+            }
+        }
+    }
+
+    @Test
+    fun testGenerateValuesProcedure() {
+        for ((_, generatorName) in propertiesForEachNode) {
+            TestUtil.testCall(
+                    EmbeddedServerHelper.graphDb, "CALL generate.values({howMany},{generatorName},{parameters})",
+                    mapOf(
+                            Pair(first = "howMany", second = howManyToCreate),
+                            Pair(first = "generatorName", second = generatorName),
+                            Pair(first = "parameters", second = arrayListOf(""))
+                    )
+            ) { result ->
+                assertThat(result.keys).contains("values")
+                assertThat(result["values"] as ArrayList<*>).hasSize(howManyToCreate)
             }
         }
     }
